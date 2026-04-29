a393a915ca711611c52d7d3b34591b18a4271eaf
Qualidade2025 <baldiqualidade@gmail.com>
2026-04-29 12:06:50 -0300

    Delete Code.js

--- Code.js
+++
@@ -1,8 +0,0 @@
-/**
- * Entry point do Web App.
- */
-function doGet() {
-  return HtmlService.createHtmlOutputFromFile('Index')
-    .setTitle('Teste Baldi')
-    .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
-}
